dd21c0bd3350bc0ef682881eac6fa37f0683a765
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-01 16:41:46 +0100

    created app and router file and required all the repositories in app

---
+++ app.rb
@@ -0,0 +1,4 @@
+require_relative 'app/repositories/uniq_email_repository.rb'
+require_relative 'app/repositories/uniq_phone_or_email_repository.rb'
+require_relative 'app/repositories/uniq_phone_repository.rb'
+require_relative 'app/repositories/all_users_repository.rb'
